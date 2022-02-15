1fc0848cc98771a07b4fc7a83d4bb0e0000bdab2
Pongchi <jwj1219j@naver.com>
2022-02-16 02:24:16 +0900

    Starting Develop!!

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 2;
+let myName = "Pongchi";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello, " + myName);
+
+myName = "Pongchi_KR";
+console.log("Your new name is " + myName);
