3d30e6727f479b6b6dceb27565d603e6872f0555
Pongchi <jwj1219j@naver.com>
2022-02-16 20:12:02 +0900

    refactoring code 1

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const h1 = document.querySelector(".hello > h1");
 
 
 function handleTitleClick() {
-    if (h1.className === "active") {
+    const clickedClass = "clicked";
+    if (h1.className === clickedClass) {
         h1.className = "";
     } else {
-        h1.className = "active";
+        h1.className = clickedClass;
     }
 }
 
