683d09bc29b6b55b0826a1b05d10f85473675f81
Pongchi <jwj1219j@naver.com>
2022-02-16 20:08:02 +0900

    mix javascript, css!
    Awesome!

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const h1 = document.querySelector(".hello > h1");
 
 
 function handleTitleClick() {
-    if (h1.style.color === "blue") {
-        h1.style.color = "tomato";
+    if (h1.className === "active") {
+        h1.className = "";
     } else {
-        h1.style.color = "blue";
+        h1.className = "active";
     }
 }
 
