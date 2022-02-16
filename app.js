277caada654d196f17bafeaa58942817176aacf4
Pongchi <jwj1219j@naver.com>
2022-02-16 20:27:20 +0900

    more code 1

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const h1 = document.querySelector(".hello > h1");
+const h1 = document.querySelector(".hello .sexy-font");
 
 
 function handleTitleClick() {
     const clickedClass = "clicked";
-    if (h1.className === clickedClass) {
-        h1.className = "";
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
     } else {
-        h1.className = clickedClass;
+        h1.classList.add(clickedClass);
     }
 }
 
