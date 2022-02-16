912b1e0d36bf6a867c0e3b99dd67881fa4dc3945
Pongchi <jwj1219j@naver.com>
2022-02-16 20:33:43 +0900

    more code 3

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const h1 = document.querySelector("div.hello:first-child h1.sexy-font");
 
 
-function handleTitleClick() {
-    h1.classList.toggle("clicked");
+function handleClick() {
+    this.classList.toggle("clicked");
 }
 
-h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("click", handleClick);
