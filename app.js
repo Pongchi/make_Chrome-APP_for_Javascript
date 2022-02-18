127a59ed333802d22d69e9fac7f4a956d6c951fe
Pongchi <jwj1219j@naver.com>
2022-02-18 11:56:03 +0900

    load Username

--- app.js
+++ app.js
@@ -3,14 +3,32 @@ const loginInput = loginForm.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
+
     const username = loginInput.value;
     loginForm.classList.add("hidden");
+    localStorage.setItem(USERNAME_KEY, username);
 
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello, ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+} else {
+    paintGreetings(savedUsername);
+}
