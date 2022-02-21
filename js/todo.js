c7ca7cefd241a9d11d8be7ad6ef11ad9333f2574
Pongchi <jwj1219j@naver.com>
2022-02-21 19:28:08 +0900

    Input ToDo-List

---
+++ js/todo.js
@@ -0,0 +1,35 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function deleteToDo(e) {
+    const li = e.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+
+    li.appendChild(span);
+    li.appendChild(button);
+
+    span.innerText = newTodo;
+    
+    toDoList.appendChild(li);
+}
+
+function handleToDoSumbit(e) {
+    e.preventDefault();
+
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+
+    paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSumbit);
