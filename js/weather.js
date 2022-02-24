0ee4fb4cda92a9a222027ff5ff8cea272dbfbebe
Pongchi <jwj1219j@naver.com>
2022-02-24 22:52:31 +0900

    add function weather

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,17 @@
+const API_KEY = '369fa3d8430a314015c8c374f3bce502';
 function onGeoOk(position) {
-    console.log(position);
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+
+    fetch(url).then(res => res.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
