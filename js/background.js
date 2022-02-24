2de5ebeb324aace4328f3b0c82e0afffb74856d6
Pongchi <jwj1219j@naver.com>
2022-02-24 23:02:20 +0900

    set Background Image

--- js/background.js
+++ js/background.js
@@ -3,4 +3,5 @@ const chosenImage = images[ Math.floor(Math.random() * images.length) ];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.className = "bgImage";
 document.body.appendChild(bgImage);
